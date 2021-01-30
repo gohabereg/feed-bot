c5975b2aadeaad7ca7082a93d1f15a94b9228697
George Berezhnoy <gohabereg@gmail.com>
2021-01-30 10:47:25 +0300

    Fix

--- bot/vk/types/link.py
+++ bot/vk/types/link.py
@@ -4,7 +4,6 @@ class Link:
     def __init__(self, api_response):
         self.url = api_response['url']
         self.title = api_response['title']
-        self.caption = api_response['caption']
         self.description = api_response['description']
         self.photo = Photo(api_response['photo'])
         if 'is_favorite' in api_response:
