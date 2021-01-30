2616720b724670340dfa1c2ed584e107f8e29f03
George Berezhnoy <gohabereg@gmail.com>
2021-01-30 06:10:21 +0300

    Update procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 -u -m bot.main
+worker: python3 -u -m bot.main
--- bot/main.py
+++ bot/main.py
@@ -26,7 +26,6 @@ from urllib.parse import urlencode
 from .vk.api import VkApi
 from .vk.scheduler import Scheduler
 from .helpers import create_reply_markup
-from .auth import AuthServer
 
 from dotenv import load_dotenv, find_dotenv
 import asyncio
@@ -59,11 +58,6 @@ class Bot:
         self.updater.start_polling()
         self.scheduler.run()
         self.updater.idle()
-
-        def cb():
-            pass
-
-        auth = AuthServer(callback=cb)
 
         pass
 
