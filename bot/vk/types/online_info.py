878e971304a5971033056fa93a00eaeb28587e13
George Berezhnoy <gohabereg@gmail.com>
2021-01-30 05:34:51 +0300

    fix

--- bot/vk/types/online_info.py
+++ bot/vk/types/online_info.py
@@ -1,5 +1,5 @@
 class OnlineInfo:
     def __init__(self, api_response):
         self.visible = api_response['visible']
-        self.is_online = api_response['is_online']
+        #self.is_online = api_response['is_online']
         self.is_mobile = api_response['is_mobile']
