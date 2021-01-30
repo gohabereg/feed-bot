5788ad716fb32153629b054d73b5f9fec6c8e6b3
George Berezhnoy <gohabereg@gmail.com>
2021-01-30 10:07:21 +0300

    Fix

--- bot/vk/types/group.py
+++ bot/vk/types/group.py
@@ -2,7 +2,6 @@ class Group:
     def __init__(self, api_response):
         self.id = api_response['id']
         self.name = api_response['name']
-        self.screen_name = api_response['screen_name']
         self.type = api_response['type']
         self.photo_50 = api_response['photo_50']
         self.photo_100 = api_response['photo_100']
--- bot/vk/types/profile.py
+++ bot/vk/types/profile.py
@@ -8,7 +8,6 @@ class Profile:
         self.id = api_response['id']
         self.last_name = api_response['last_name']
         self.sex = Sex(api_response['sex'])
-        self.screen_name = api_response['screen_name']
         self.photo_50 = api_response['photo_50']
         self.photo_100 = api_response['photo_100']
         self.online_info = OnlineInfo(api_response['online_info'])
